f8b469f489a55c43c49a0c18e8afee7ba205fc2d
Jatin <Jatin397911@gmail.com>
2026-07-14 11:44:25 +0530

    loginUser LogoutUser getUSerProfile BlacklistModel authenticateToken Middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser} = require("../controllers/auth.controller");
-
+const { authenticateToken } = require("../middleware/auth.middleware");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUserProfile,
+} = require("../controllers/auth.controller");
 
 const User = require("../models/user.model");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
 
 router.post("/register", registerUser);
-
-
-
-
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.get("/profile", authenticateToken, getUserProfile);
 
 module.exports = router;
