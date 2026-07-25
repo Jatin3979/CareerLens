5549590cc079f1a21b998f2ec24c4f31fee69150
Jatin <Jatin397911@gmail.com>
2026-07-25 10:46:21 +0530

    Fixed genreatePdf for deployment

--- Backend/src/services/ai.services.js
+++ Backend/src/services/ai.services.js
@@ -158,7 +158,9 @@ async function generateInterviewReport({
 }
 
 async function generatePdfFromHtml(htmlContent) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
   const pdfBuffer = await page.pdf({
